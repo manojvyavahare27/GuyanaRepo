f1bef30545805c50cc444c569b67bcab8225d88c
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-11-24 07:16:05 +0530

    Push Project code to git

---
+++ Pages/PatientDomain/ViewPIP.js
@@ -0,0 +1,30 @@
+class ViewPIP
+{
+    constructor(page)
+    {
+        this.page=page
+        //this.linkViewPIP=page.getByTestId('View')
+       // this.linkViewPIP=page.getByRole('cell', { name: 'View' }).getByTestId('View')
+       this.linkViewPIP=page.locator("xpath=//a[@data-testid='View']")
+        this.btnCloseViewPopup=page.getByTestId('CancelIcon')
+        this.btnNextOnViewPIP=page.locator("xpath=//button[@data-testid='Next']")
+        this.txtboxSearchPIP=page.locator("xpath=//button[@data-testid='Search']")
+    }
+    async EnterPIPInSearch(name)
+    {
+        await this.txtboxSearchPIP.type(name)
+    }
+    async clickOnNextbntViewPIP()
+    {
+        await this.btnNextOnViewPIP.click()
+    }
+    async clickOnCloseViewPopup()
+    {
+        await this.btnCloseViewPopup.click()
+    }
+    async clickOnViewPIPLink()
+    {
+        await this.linkViewPIP.click()
+    }
+}
+module.exports=ViewPIP
--- playwright.config.js
+++ playwright.config.js
@@ -1,32 +1,49 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+const { defineConfig, devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
+module.exports = defineConfig({
+  //globalSetup: require.resolve("./config/global-setup"),
   testDir: './tests',
+  
+
+  /* Maximum time one test can run for. */
+  timeout: 600 * 10000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 8000
+  },
   /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source x  code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+  // reporter: 'html',
+  reporter: [ ['html']
+              //['allure-playwright',{outputFolder: 'my-allure-result'}]
+
+            ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -36,18 +53,58 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'chrome',
+      use: {
+        //...devices['Desktop Chrome'],
+        screenshot:"on",
+        video:"on",
+        trace:"on",
+        headless:false,
+     // storageState:'Shree.json',
+        reporter: [["dot"],
+                  ["json", { outputFile: "test-result.json" }],
+                  ['allure-playwright', {outputFile: 'my-allure-results'}]
+                ],
+       // ['experimental-allure-playwright']],
+       viewport: null,
+        launchOptions:{
+         //slowMo:1000,
+         args: ['--start-maximized']
+        }
+      }
+    },
+
+    // {
+    //   name: 'Firefox',
+    //   use: { ...devices['Desktop Firefox'] ,
+    //   screenshot:"on",
+    //     video:"on",
+    //     trace:"on",
+    //     headless:false,
+    //     storageState:'ExcelToJSON.json',
+    //     reporter: [["dot"], ["json", { outputFile: "test-result.json" }],
+        
+    //     ['experimental-allure-playwright']],
+    //     launchOptions:{
+    //      // slowMo:500
+    //     }
+    //   }
     // },
 
     // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   name: 'Edge',
+    //    use: { channel: 'msedge' ,
+    //     screenshot:"on",
+    //     video:"on",
+    //     trace:"on",
+    //     headless:false,
+    //    // storageState:'Shree.json',
+    //     reporter: [["dot"], ["json", { outputFile: "test-result.json" }],
+    //     ['experimental-allure-playwright']],
+    //     launchOptions:{
+    //      // slowMo:500
+    //     }
+    //   }
     // },
 
     /* Test against mobile viewports. */
@@ -63,19 +120,21 @@ export default defineConfig({
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //   use: { channel: 'msedge' },
     // },
     // {
     //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   use: { channel: 'chrome' },
     // },
   ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //   port: 3000,
   // },
 });
 
